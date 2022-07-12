368cba749a2e7f5238c6cfe7fa7d072c214289fc
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-07-12 19:56:06 +0600

    Javascript OOP Inheritance

---
+++ app.js
@@ -0,0 +1,29 @@
+//OOP Inheritance
+
+//Example-01
+// class employee{
+//     constructor(){
+//         console.log("Constructor: Employee");
+//     }
+// }
+// class manager extends employee{}
+// let a = new employee();
+
+
+//Example-02
+class employee{
+    constructor(){
+        console.log("Constructor: Manager");
+    }
+}
+class manager extends employee{}
+let b = new manager();
+
+
+
+
+
+
+
+
+
